9d6670cd185881285aaf4766082941edb946309f
Giulio Calzolari <giulio.calzolari@cloudreach.co.uk>
2015-09-14 12:58:41 +0200

    update

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -21,6 +21,9 @@ EOM
 
 VERBOSE=false
 IMAGE=false
+
+NOW=$(date +"%Y-%m-%d %T")
+
 if [ -z ${DOCKER_KEY+x} ]; then DOCKER_KEY=false; fi
 
 # Loop through arguments, two at a time for key and value
@@ -56,35 +59,40 @@ fi
 
 
 if [ $DOCKER_KEY == false ]; then
-    echo "DOCKER_KEY is required. You can set it as an environment variable or pass the value using -k or --docker-key"
+    echo "[$NOW] DOCKER_KEY is required. You can set it as an environment variable or pass the value using -k or --docker-key"
     exit 1
 fi
 
 if [ $IMAGE == false ]; then
-    echo "IMAGE is required. You can pass the value using -i or --image"
+    echo "[$NOW] IMAGE is required. You can pass the value using -i or --image"
     exit 1
 fi
 
 
+
 # trigger build
-echo "trigger build to Hub Docker"
+echo "[$NOW] trigger build to Hub Docker"
 curl -H "Content-Type: application/json" --data '{"build": true}' -X POST "https://registry.hub.docker.com/u/$IMAGE/trigger/$DOCKER_KEY/"
 echo ""
-
+sleep 5
 STATUS=0
 
 # wait until build is ready
-while [ $STATUS -gt 0 ]; do
-  sleep 5
+while [ $STATUS > 0 ]
+do
   NOW=$(date +"%Y-%m-%d %T")
   STATUS=$(curl -s  https://hub.docker.com/v2/repositories/$IMAGE/buildhistory/?page_size=1 | jq .results[0].status)
   echo "[$NOW] Wainting Docker Image... Code:$STATUS"
-done
 
-  if [ $STATUS == 10 ]; then
+
+  if [[ $STATUS == 10 ]]; then
     echo "[$NOW] Build OK"
     exit 0
   else
     echo "[$NOW] Build Error Code: $STATUS"
     exit 1
   fi
+
+  sleep 5
+
+done
